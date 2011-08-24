7ba5a899cdfc859309c376050df0141d3a9c162e
Robert Wohlfarth <robert.j.wohlfarth@vanderbilt.edu>
2011-08-24 16:24:00 -0500

    Default the MS Access connection to allowing long Memo fields.

--- lib/ETL/Load/Access.pm
+++ lib/ETL/Load/Access.pm
@@ -35,6 +35,45 @@ augment 'connect' => sub {
 };
 
 
+=head3 around connect()
+
+MS Access does not handle Memo fields very well through the ODBC interface. 
+This code sets the connection for the longer text. To disable this feature,
+see the L</memo_length> attribute. 
+
+=cut
+
+around 'connect' => sub {
+	my $original = shift @_;
+	my $self     = shift @_;
+	
+	my $dbh = $original->( $self, @_ );
+	if (defined( $dbh ) and defined( $self->memo_length )) {
+		$dbh->{LongReadLen} = $self->memo_length;
+		$dbh->{LongTruncOk} = 1;
+	}
+	return $dbh;
+};
+
+
+=head3 memo_length
+
+MS Access does not handle Memo fields very well through the ODBC interface. The 
+L</around connect()> code configures DBI for text up to this many characters.
+The default value is quite high (262,144). Your application may change it 
+B<before> it saves the first record.
+
+To disable memo fields altogether, set this attribute to C<undef>.
+
+=cut
+
+has memo_length => (
+	default => 262144,
+	is      => 'rw',
+	isa     => 'Maybe[Int]',
+);
+
+
 =head1 SEE ALSO
 
 L<ETL::Load>, L<ETL::Load::DBI>, L<ETL::Record>
